01ac9a9c378b2221fc3afb648fe5e4a65ca4c68e
Adam Harris <adam@turnkeysocial.com>
2013-09-13 14:15:32 -0600

    Fix directory structure, create plugin.xml, and update readme

--- SMSExample/assets/www/smsplugin.js
+++
@@ -1,9 +0,0 @@
-var SmsPlugin = function () {};
-
-SmsPlugin.prototype.send = function (phone, message, method, successCallback, failureCallback) {    
-    return PhoneGap.exec(successCallback, failureCallback, 'SmsPlugin', "SendSMS", [phone, message, method]);
-};
-
-PhoneGap.addConstructor(function() {
-    PhoneGap.addPlugin("sms", new SmsPlugin());
-});
