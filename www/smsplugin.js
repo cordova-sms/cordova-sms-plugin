f5486e5ec6c1c6e282e05d80c91209c2bd45c5d8
Adam Harris <adam@turnkeysocial.com>
2013-09-13 16:28:00 -0600

    Change js file to use cordova.exec

--- www/smsplugin.js
+++ www/smsplugin.js
@@ -1,9 +1,16 @@
+var exec = require("cordova/exec");
 var SmsPlugin = function () {};
 
-SmsPlugin.prototype.send = function (phone, message, method, successCallback, failureCallback) {    
+SmsPlugin.prototype.send = function (phone, message, method, successCallback, failureCallback) {
+    return cordova.exec(successCallback, failureCallback, 'SmsPlugin', "SendSMS", [phone, message, method]);
+};
+
+window.sms = new SmsPlugin();
+
+/*SmsPlugin.prototype.send = function (phone, message, method, successCallback, failureCallback) {
     return PhoneGap.exec(successCallback, failureCallback, 'SmsPlugin', "SendSMS", [phone, message, method]);
 };
 
 PhoneGap.addConstructor(function() {
     PhoneGap.addPlugin("sms", new SmsPlugin());
-});
+});*/
