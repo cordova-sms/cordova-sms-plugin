2fbd61b47b26c36ca7aa700ed75c507a6c256f62
aharris88 <aharris88@gmail.com>
2013-09-16 17:21:52 -0600

    Update name values

--- www/smsplugin.js
+++ www/smsplugin.js
@@ -1,3 +1,4 @@
+
 var exec = require("cordova/exec");
 var SmsPlugin = function () {};
 
@@ -5,7 +6,8 @@ SmsPlugin.prototype.send = function (phone, message, method, successCallback, fa
     exec(successCallback, failureCallback, 'SmsPlugin', "SendSMS", [phone, message, method]);
 };
 
-window.sms = new SmsPlugin();
+module.exports = new SmsPlugin();
+//window.sms = new sms();
 
 /*SmsPlugin.prototype.send = function (phone, message, method, successCallback, failureCallback) {
     return PhoneGap.exec(successCallback, failureCallback, 'SmsPlugin', "SendSMS", [phone, message, method]);
