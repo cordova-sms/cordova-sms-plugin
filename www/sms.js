e8398b179ac9f9377b78b512997569384f018b0d
Adam Harris <adam@turnkeysocial.com>
2013-09-18 13:36:08 -0600

    Fix class not found error

--- www/sms.js
+++ www/sms.js
@@ -1,31 +1,15 @@
 
 var sms = {
-    send: function (phone, message, method, successCallback, failureCallback) {
+    send: function(phone, message, method, successCallback, failureCallback) {
         alert("sms.send");
-        cordova.eexec(
-        	successCallback,
-        	failureCallback,
-        	'Sms',
-        	"send",
-        	[phone, message, method]
+        cordova.exec(
+            successCallback,
+            failureCallback,
+            'Sms',
+            'send',
+            [phone, message, method]
         );
     }
 }
 
-/*var exec = require("cordova/exec");
-var SmsPlugin = function () {};
-
-SmsPlugin.prototype.send = function (phone, message, method, successCallback, failureCallback) {
-    exec(successCallback, failureCallback, 'SmsPlugin', "SendSMS", [phone, message, method]);
-};
-
-module.exports = new SmsPlugin();*/
-//window.sms = new sms();
-
-/*SmsPlugin.prototype.send = function (phone, message, method, successCallback, failureCallback) {
-    return PhoneGap.exec(successCallback, failureCallback, 'SmsPlugin', "SendSMS", [phone, message, method]);
-};
-
-PhoneGap.addConstructor(function() {
-    PhoneGap.addPlugin("sms", new SmsPlugin());
-});*/
+module.exports = sms;
