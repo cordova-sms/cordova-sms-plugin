4fa86080eba72f62f24cc378f8648505e872f387
Fredrik Eldh <fredrik.eldh@evothings.com>
2014-01-15 11:57:05 +0100

    Added Windows Phone 8 support.

---
+++ src/wp8/Sms.cs
@@ -0,0 +1,23 @@
+using System;
+using Microsoft.Phone.Tasks;
+using WPCordovaClassLib.Cordova;
+using WPCordovaClassLib.Cordova.Commands;
+using WPCordovaClassLib.Cordova.JSON;
+
+namespace WPCordovaClassLib.Cordova.Commands
+{
+	public class Sms : BaseCommand
+	{
+		public void send(string options)
+		{
+			string[] optValues = JsonHelper.Deserialize<string[]>(options);
+			String number = optValues[0];
+			String message = optValues[1];
+
+			SmsComposeTask task = new SmsComposeTask();
+			task.Body = message;
+			task.To = number;
+			task.Show();
+		}
+	}
+}
