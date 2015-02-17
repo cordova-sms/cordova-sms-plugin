5bc4a5071b07f18240136225c31067b432cdb59a
dbaq <didier.baquier@myerp.com>
2015-02-17 14:49:40 -0800

    add the new parameter to the list of arguments to send to the plugin

--- www/sms.js
+++ www/sms.js
@@ -20,12 +20,14 @@ sms.send = function(phone, message, options, success, failure) {
     phone = convertPhoneToArray(phone);
 
     // parsing options
+    var replaceLineBreaks = false;
     var androidIntent = '';
     if (typeof options === 'string') { // ensuring backward compatibility
         window.console.warn('[DEPRECATED] Passing a string as a third argument is deprecated. Please refer to the documentation to pass the right parameter: https://github.com/cordova-sms/cordova-sms-plugin.');
         androidIntent = options;
     }
     else if (typeof options === 'object') {
+        replaceLineBreaks = options.replaceLineBreaks || false;
         if (options.android && typeof options.android === 'object') {
             androidIntent = options.android.intent;
         }
@@ -36,7 +38,7 @@ sms.send = function(phone, message, options, success, failure) {
         success,
         failure,
         'Sms',
-        'send', [phone, message, androidIntent]
+        'send', [phone, message, androidIntent, replaceLineBreaks]
     );
 };
 
