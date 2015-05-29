6ad5f5ae61d7845ccef7cc04707662f9ebce3af0
dbaq <didier.baquier@myerp.com>
2015-05-29 14:31:01 -0700

    restructure js code

--- www/sms.js
+++ www/sms.js
@@ -2,45 +2,36 @@
 
 var exec = require('cordova/exec');
 
-var sms = {};
+var sms = function() {
 
-function convertPhoneToArray(phone) {
-    if (typeof phone === 'string' && phone.indexOf(',') !== -1) {
-        phone = phone.split(',');
-    }
-    if (Object.prototype.toString.call(phone) !== '[object Array]') {
-        phone = [phone];
-    }
-    return phone;
-}
+    var parseOptions = function(options) {
+        var opts = {
+            attachments: null,
+            replaceLineBreaks: false,
+            android: {
+                intent: 'INTENT'
+            }
+        };
 
-
-sms.send = function(phone, message, options, success, failure) {
-    // parsing phone numbers
-    phone = convertPhoneToArray(phone);
-
-    // parsing options
-    var replaceLineBreaks = false;
-    var androidIntent = '';
-    if (typeof options === 'string') { // ensuring backward compatibility
-        window.console.warn('[DEPRECATED] Passing a string as a third argument is deprecated. Please refer to the documentation to pass the right parameter: https://github.com/cordova-sms/cordova-sms-plugin.');
-        androidIntent = options;
-    }
-    else if (typeof options === 'object') {
-        replaceLineBreaks = options.replaceLineBreaks || false;
-        if (options.android && typeof options.android === 'object') {
-            androidIntent = options.android.intent;
+        if (typeof options === 'string') { // ensuring backward compatibility
+            window.console.warn('[DEPRECATED] Passing a string as a third argument is deprecated. Please refer to the documentation to pass the right parameter: https://github.com/cordova-sms/cordova-sms-plugin.');
+            opts.android.intent = options;
+        } else if (typeof options === 'object') {
+            opts.replaceLineBreaks = options.replaceLineBreaks || false;
+            opts.attachments = options.attachments || false;
+            if (options.android && typeof options.android === 'object') {
+                opts.android.intent = options.android.intent;
+            }
         }
-    }
 
-    // fire
-    exec(
-        success,
-        failure,
-        'Sms',
-        'send', [phone, message, androidIntent, replaceLineBreaks]
-    );
-};
+        return opts;
+    };
 
+    return {
+        send: function(recipients, message, options, success, failure) {
+            return exec(success, failure, 'Sms', 'send', [recipients, message, parseOptions(options)]);
+        }
+    };
+}();
 
 module.exports = sms;
