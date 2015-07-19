82a108b4f9c400dd48f1d21a3b671234f1946314
dbaq <didier.baquier@myerp.com>
2015-07-19 10:32:22 -0700

    iOS: run the plugin in a background thread

--- src/ios/Sms.m
+++ src/ios/Sms.m
@@ -10,42 +10,44 @@
 }
 
 - (void)send:(CDVInvokedUrlCommand*)command {
-    
-    self.callbackID = command.callbackId;
-    
-    if(![MFMessageComposeViewController canSendText]) {
-        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Notice"
-                                                        message:@"SMS Text not available."
-                                                       delegate:self
-                                              cancelButtonTitle:@"OK"
-                                              otherButtonTitles:nil
-                              ];
-        [alert show];
-        return;
-    }
-    
-    MFMessageComposeViewController *composeViewController = [[MFMessageComposeViewController alloc] init];
-    composeViewController.messageComposeDelegate = self;
-    
-    NSString* body = [command.arguments objectAtIndex:1];
-    if (body != nil) {
-        BOOL replaceLineBreaks = [[command.arguments objectAtIndex:3] boolValue];
-        if (replaceLineBreaks) {
-            body = [body stringByReplacingOccurrencesOfString: @"\\n" withString: @"\n"];
-        }
-        [composeViewController setBody:body];
-    }
-    
-    NSMutableArray* recipients = [command.arguments objectAtIndex:0];
-    if (recipients != nil) {
-        if ([recipients.firstObject isEqual: @""]) {
-            [recipients replaceObjectAtIndex:0 withObject:@"?"];
+    [self.commandDelegate runInBackground:^{
+        self.callbackID = command.callbackId;
+        
+        if(![MFMessageComposeViewController canSendText]) {
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Notice"
+                                                            message:@"SMS Text not available."
+                                                           delegate:self
+                                                  cancelButtonTitle:@"OK"
+                                                  otherButtonTitles:nil
+                                  ];
+
+            [alert show];
+            return;
         }
         
-        [composeViewController setRecipients:recipients];
-    }
-    
-    [self.viewController presentViewController:composeViewController animated:YES completion:nil];
+        MFMessageComposeViewController *composeViewController = [[MFMessageComposeViewController alloc] init];
+        composeViewController.messageComposeDelegate = self;
+        
+        NSString* body = [command.arguments objectAtIndex:1];
+        if (body != nil) {
+            BOOL replaceLineBreaks = [[command.arguments objectAtIndex:3] boolValue];
+            if (replaceLineBreaks) {
+                body = [body stringByReplacingOccurrencesOfString: @"\\n" withString: @"\n"];
+            }
+            [composeViewController setBody:body];
+        }
+        
+        NSMutableArray* recipients = [command.arguments objectAtIndex:0];
+        if (recipients != nil) {
+            if ([recipients.firstObject isEqual: @""]) {
+                [recipients replaceObjectAtIndex:0 withObject:@"?"];
+            }
+            
+            [composeViewController setRecipients:recipients];
+        }
+        
+        [self.viewController presentViewController:composeViewController animated:YES completion:nil];
+    }];
 }
 
 #pragma mark - MFMessageComposeViewControllerDelegate Implementation
