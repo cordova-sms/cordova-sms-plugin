175014804adcb374660db9bbd4dabe2cff689416
rk-7 <30998401+rk-7@users.noreply.github.com>
2018-08-02 16:38:47 +0530

    Added missing statements to the fix

--- src/windows/SmsProxy.js
+++ src/windows/SmsProxy.js
@@ -4,7 +4,10 @@ module.exports = {
 
         var chatMessage = new Windows.ApplicationModel.Chat.ChatMessage();
         chatMessage.body = args[1];
-        if (args[0]) {
+        const validRecipent = args[0].every(function (r) {
+            return !r == false;
+        });
+        if (validRecipent) {
             chatMessage.recipients.push(args[0]);
         }
         Windows.ApplicationModel.Chat.ChatMessageManager.showComposeSmsMessageAsync(chatMessage).done(win, fail);
